f6b9363f4e3e1e295872ac8f226cb8599d028a91
Arnar Snaer <arnarsnaer98@gmail.com>
2018-09-04 14:59:23 +0000

    Completed program

--- sequence.py
+++ sequence.py
@@ -1,3 +1,16 @@
 # Find sequence of 1, 2, 3, 6, 11, 20, 37, .. , ..
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+last_num = 0
+last_num2 = 0
+num = 1
+
+for x in range(n):
+    summa = 0
+    summa = last_num2 + last_num + num
+    if summa >= 3:
+        last_num2 = last_num
+    last_num = num
+    num = summa
+    print(summa)
